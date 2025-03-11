ec35dbb51e43a399e7801e3728471214a7c34221
Xinxin Liu <xxliu95@gmail.com>
2025-03-12 00:40:27 +0100

    Add style, navbar and more

--- app/forms.py
+++ app/forms.py
@@ -5,3 +5,8 @@ class Entrada(forms.ModelForm):
     class Meta:
         model = Texto
         fields = ['numero']
+
+    def __init__(self, *args, **kwargs):
+        super(Entrada, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
--- app/utils.py
+++ app/utils.py
@@ -5,7 +5,7 @@ def numero_a_letras(numero):
     if "." in numero:
         entero, decimal = numero.split(".")
         entero_letras = num2words(int(entero), lang="es")
-        decimal_letras = " ".join(num2words(int(digito), lang="es") for digito in decimal)
+        decimal_letras = num2words(int(decimal), lang="es")
         return f"{entero_letras} con {decimal_letras}"
     else:
         return num2words(int(numero), lang="es")
